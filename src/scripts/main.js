870cb193754a4dd8b6187b08e6cb5a2e901c43ec
mpodaniev <mpodanev@gmail.com>
2025-11-16 08:06:49 +0200

    Sort employee list by salary and update DOM dynamically

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write code here
+const getEmployees = (listElement) => {
+  return [...listElement].map((employee) => {
+    return {
+      name: employee.innerText,
+      position: employee.dataset.position,
+      salary: employee.dataset.salary,
+      age: employee.dataset.age,
+    };
+  });
+};
+
+const sortList = (list) => {
+  const listElement = document.getElementsByTagName('ul')[0];
+  const employees = getEmployees(listElement.children);
+  const sortedEmployees = employees.sort(
+    (a, b) =>
+      Number(b.salary.slice(1).replace(',', '')) -
+      Number(a.salary.slice(1).replace(',', '')),
+  );
+
+  listElement.innerHTML = sortedEmployees
+    .map(
+      (employee) =>
+        `<li data-salary="${employee.salary}" data-position="${employee.position}" data-age="${employee.age}">${employee.name}</li>`,
+    )
+    .join('');
+};
+
+sortList('ul');
